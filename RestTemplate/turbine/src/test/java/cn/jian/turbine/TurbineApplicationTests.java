0ec9cd2ecff4e9879ab0f54c3b7d67d92fb6afbe
lijian12345 <1053294286@qq.com>
2020-12-05 17:20:10 +0800

    turbine

---
+++ RestTemplate/turbine/src/test/java/cn/jian/turbine/TurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jian.turbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
