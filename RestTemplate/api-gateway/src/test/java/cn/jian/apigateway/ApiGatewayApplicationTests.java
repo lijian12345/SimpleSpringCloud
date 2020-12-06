bac49df98acc87251864d6628e3580749a31fff6
lijian12345 <1053294286@qq.com>
2020-12-06 14:59:08 +0800

    zuul

---
+++ RestTemplate/api-gateway/src/test/java/cn/jian/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jian.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
